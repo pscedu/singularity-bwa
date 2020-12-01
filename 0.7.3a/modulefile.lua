53794569b069fe2e5e7c8136627223e33b28b426
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-11-30 19:01:15 -0500

    Updated repo for Bridges2

---
+++ 0.7.3a/modulefile.lua
@@ -0,0 +1,51 @@
+--
+-- bwa 0.7.3a modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: BWA is a software package for mapping low-divergent sequences against a large reference genome, such as the human genome."
+-- "Keywords: singularity bioinformatics"
+
+whatis("Name: bwa")
+whatis("Version: 0.7.3a")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: BWA is a software package for mapping low-divergent sequences against a large reference genome, such as the human genome.")
+
+help([[
+bwa 0.7.3a
+--------------
+
+Description
+-----------
+BWA is a software package for mapping low-divergent sequences against a large reference genome, such as the human genome.
+
+To load the module type
+
+> module load bwa/0.7.3a
+
+To unload the module type
+
+> module unload bwa/0.7.3a
+
+Documentation
+-------------
+http://bio-bwa.sourceforge.net/
+
+For help, type (no input arguments)
+
+> bwa
+
+Repository
+----------
+https://github.com/lh3/bwa
+
+Tools included in this module are
+
+* bwa
+]])
+
+local package = "bwa"
+local version = "0.7.3a"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
